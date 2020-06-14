b2e523de341ef17f8918be06f02faf164283aa0e
ankitprakash16cse <ankit.prakash.16cse@bml.edu.in>
2020-06-14 22:14:37 +0530

    my commit

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import UserForm from './component/UserForm';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <UserForm/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    justifyContent:'center',
+    backgroundColor:'#36485f',
+    paddingLeft:60,
+    paddingRight:60
+    
+    
   },
 });
